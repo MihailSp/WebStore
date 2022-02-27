d193f14c066ea240160b74609c9bc38379b208ae
MihailSp <kormhij@yandex.ru>
2022-02-27 21:52:59 +0500

    представление на сотрудников

--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace WebStore.Models
+﻿using System.Text;
+
+namespace WebStore.Models
 {
     public class Employee
     {
@@ -10,6 +12,29 @@
 
         public string Patronymic { get; set; }
 
-        public int Age { get; set; }
+        public string ShortName { 
+            get
+            {
+                var result = new StringBuilder(LastName);
+                if(FirstName is {Length: >0} first_name)
+                    result.Append(".").Append(first_name[0]).Append(".");
+                if (Patronymic is { Length: > 0 } patronymic)
+                    result.Append(patronymic[0]).Append(".");
+
+                return result.ToString();
+            }              
+        }
+
+        private int _Age;
+
+        public int Age {
+            get => _Age;
+            set
+            {
+                if (value <=0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Значение возраста не может быть меньше 0");
+                _Age = value;
+            }
+        }
     }
 }
