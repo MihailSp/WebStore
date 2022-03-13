3a150a8cc2d70deca624f2573869e257d873b239
MihailSp <kormhij@yandex.ru>
2022-03-14 04:07:32 +0500

    соглашения

---
+++ WebStore/Infrastructure/Conventions/TestConvention.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace WebStore.Infrastructure.Conventions
+{
+    public class TestConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            if(controller.ControllerName == "Home")
+            {
+
+            }
+        }
+    }
+}
