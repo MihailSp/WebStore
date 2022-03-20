1ff6828f891873f27ff6205de9dce0d87020615b
MihailSp <kormhij@yandex.ru>
2022-03-20 22:56:28 +0500

    Редактирование сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@
 using WebStore.Data;
 using WebStore.Models;
 using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -29,6 +30,50 @@ namespace WebStore.Controllers
             if(employee == null) return NotFound();
 
             return View(employee);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _EmployeesData.GetById(id);
+            if (employee == null) return NotFound();
+
+            var model = new EmployeesViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                FirstName = employee.FirstName,
+                ShortName = employee.ShortName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeesViewModel Model)
+        {
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                FirstName = Model.FirstName,
+                LastName = Model.LastName,
+                Age = Model.Age,
+            };  
+            
+            _EmployeesData.Edit(employee);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
         }
     }
 }
