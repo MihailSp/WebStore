0daecf5d95378bafec6389de49cbf3b705afbd85
MihailSp <kormhij@yandex.ru>
2022-05-02 15:29:10 +0500

    Логика и визуальная часть компонента брендов

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components;
 
 public class BrandsViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke() => View();
+    private readonly IProductData _ProductData;
+
+    public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+    public IViewComponentResult Invoke() => View(GetBrends());
+
+    private IEnumerable<BrendViewModel> GetBrends() => _ProductData.GetBrands()
+        .OrderBy(b => b.Order)
+        .Select(b => new BrendViewModel
+        {
+            Id = b.Id,
+            Name = b.Name,
+        });
 }
 
---
+++ WebStore/ViewModels/BrendViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.ViewModels;
+
+public class BrendViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { set; get; } = null!;
+}
+
